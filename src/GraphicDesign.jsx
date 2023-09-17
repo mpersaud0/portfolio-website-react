96c0ebadba562991c4dc6af6cf2222590b83d0b2
mpersaud0 <mikey.walkerrr@gmail.com>
2023-09-17 12:37:41 -0400

    added link to graphic design portfolio

--- src/GraphicDesign.jsx
+++ src/GraphicDesign.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 const GraphicDesign = ({props}) =>{
     return(
         <div className='GraphicDesign'>
-            <p>GraphicDesign</p>
+            <div className='projects'>
+                <div className='northStage'>
+                    <h1>North Stage Merch</h1>
+                    <h2>All social media branding, logo design, merch design, flyer design, video editing and anything visual for North Stage was created by me.</h2>
+                    <div className='imageGrid'>
+                        <img src='images/northstage/2022allstar.jpg'></img>
+                        <img src='images/northstage/nstage2023flyer.jpg'></img>
+                        <img src='images/northstage/shirt.jpg'></img>
+                        <img src='images/northstage/stickers.jpg'></img>
+                    </div>
+                </div>
+            </div>
         </div>
     )
   }
